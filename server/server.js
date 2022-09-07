494099cfc0270130abe5017bf114906a7e291a78
Navaneeth Devan <navaneethdevanofficial@gmail.com>
2022-09-07 15:19:54 +0530

    bot initialised

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
-const TelegramBot = require('node-telegram-bot-api')
+
+const { Telegraf } = require('telegraf');
+
 const dotenv = require('dotenv').config()
 
+const bot = new Telegraf(process.env.token)
 
-const token =process.env.Token
 
-console.log(token);
+bot.use((ctx,next) => {
+    ctx.reply("Enter Your first name")
+    ctx.state.firstname=ctx.message.text
+    next(ctx)
+})
 
-const bot = new TelegramBot(token, {polling: true});
+bot.use((ctx,next) => {
+    ctx.reply("Enter Your LastName")
+    ctx.state.lastName=ctx.message.text
+    next(ctx)
+})
+
+bot.use((ctx,next) => {
+    ctx.sendMessage(`${ctx.state.firstname}${ctx.state.lastName}`)
+    next(ctx)
+})
+
+bot.launch();
+
+
